c3b063077364e91a6beb2e2a97161a1b5c27a13b
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-05-17 14:07:46 +0300

    Prettier settings, basic text file reading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const handleFileOpen = (event) => {
+    const reader = new FileReader()
+    reader.onload = async (event) => {
+      const text = event.target.result
+      console.log(text)
+    }
+    reader.readAsText(event.target.files[0])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" onChange={handleFileOpen}></input>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
