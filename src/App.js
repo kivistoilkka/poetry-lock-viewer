c90999ef0b12e1516391569fe557a416cb1e373a
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-05-17 18:09:29 +0300

    Package names as list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import { useState } from 'react'
 
 const App = () => {
+  const [packages, setPackages] = useState([])
+
+  const parseTOML = (text) => {
+    const packagePart = text.split('[metadata]')[0]
+    const packages = packagePart.split('[[package]]').splice(1)
+    return packages.map((pckg) => {
+      const lines = pckg.split(/\r\n | \n/)
+      const name = lines[0].split(' = "')[1].split('"')[0]
+      return { name: name, info: pckg }
+    })
+  }
+
   const handleFileOpen = (event) => {
     const reader = new FileReader()
-    reader.onload = async (event) => {
+    reader.onload = (event) => {
       const text = event.target.result
-      console.log(text)
+      setPackages(parseTOML(text))
     }
     reader.readAsText(event.target.files[0])
   }
@@ -13,6 +25,11 @@ const App = () => {
   return (
     <div>
       <input type="file" onChange={handleFileOpen}></input>
+      <p>
+        {packages.map((pckg) => (
+          <li key={pckg.name}>{pckg.name}</li>
+        ))}
+      </p>
     </div>
   )
 }
